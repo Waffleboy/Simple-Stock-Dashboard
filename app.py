cf628b45a69386d9331979f3e25db263cc271306
waffleboy <thiru1337@gmail.com>
2016-06-11 01:41:00 +0800

    some restructuring of code, added docs

--- app.py
+++ app.py
@@ -4,46 +4,48 @@ Created on Sun Jun  5 15:54:03 2016
 
 @author: waffleboy
 """
-#TODO:
-# chart
-# highstock
-# real time
-# colum nnames
-
-from flask import Flask, render_template, url_for, send_from_directory,Blueprint
-from flask import make_response, request, current_app  
-from functools import update_wrapper
+from flask import Flask, render_template
 from pandas.io.data import DataReader
 from datetime import datetime
 from datetime import timedelta
-import chartkick
 import pandas as pd
 import pickle,json
 from pandas_highcharts.core import serialize
+from collections import OrderedDict
 
-app = Flask(__name__)
+app = Flask(__name__) #initialize app
 
-ck = Blueprint('ck_page', __name__, static_folder=chartkick.js(), static_url_path='/static')
-app.register_blueprint(ck, url_prefix='/ck')
-app.jinja_env.add_extension("chartkick.ext.charts")
-
+# Main function - Displays the data from masterDic, which contains all the information
+# to make the dashboard.
 @app.route("/")
 def main():
     masterDic = loadData(years=5)
     return render_template('main.html',masterDic=masterDic)
 
-# of form name/boughtprice
+# generates the master dictionary that contains all information for text and graph
+# Input: <int> years: number of years worth of data to show
+# Output: <dictionary> masterDic: dictionary of dictionaries. Example:
+#   {'chart0': {'textdata': {'sbux': {'Ask':3,'Bid':4,..}} ,'content':{<highcharts constructor>}}}
 def loadData(years):
     df = pd.read_csv('input.csv')
-    companyNames = list(df['stockname'])
-    query,additionalOptions = getQuery(companyNames)
-    queryDF = pd.read_csv(query,header=None).fillna('NA')
-    columnNames = getColumnNames(additionalOptions)
-    queryDF.columns = columnNames #get additional info.
-    
-    col = queryDF.set_index('Symbol').T.to_dict()
-    masterDic = {}
-    counter=0
+    companySym = list(df['stockname'])
+    query,additionalOptions = getQuery(companySym) #generate link to query from, column names to map
+    queryDF = pd.read_csv(query,header=None).fillna('NA') #actual query
+    columnNames = getColumnNames(additionalOptions) 
+    queryDF.columns = columnNames #set columm names to actual names of options
+    col = queryDF.set_index('Symbol').T.to_dict() #make dictionary of key: symbol, value: everything in the row
+    masterDic = populateMasterDic(df,col,years,OrderedDict()) #populate an orderedDict with data
+    return masterDic
+
+# Used by loadData(), fills the ordered dict with information
+# Input:
+# 1. <pandas dataframe> df: input.csv
+# 2. <dictionary> col: dictionary containing all text data for every stock
+# 3. <int> years: number of years worth of data to show
+# 4. <OrderedDict> masterDic: an orderedDict to populate data with
+# Output:
+# 1. <OrderedDict> masterDic: masterdic populated with information
+def populateMasterDic(df,col,years,masterDic):
     for i in df.index:
         name=df['stockname'][i]
         boughtprice = df['boughtprice'][i]
@@ -59,38 +61,53 @@ def loadData(years):
             data['bought'] = bought
         else:
             data = pd.DataFrame(data)
-        content = serialize(data,render_to='chart'+str(counter), title=name.upper()+' Stock',output_type='json')
+        content = serialize(data,render_to='chart'+str(i), title=name.upper()+' Stock',output_type='json')
         content = changeChartOptions(content)
-        masterDic['chart'+str(counter)] = {'id':i,
-                                            'name':name,
-                                            'textdata':col[name],
-                                          'content':content}
-        counter+=1
+        masterDic['chart'+str(i)] = {'textdata':col[name],'content':content}
     return masterDic
-
+    
+# This function adds additional options into the HighCharts dictionary
+# Input: <json> content: json that will be passed to highcharts
+# Output: <json> content: modified json to be passed to highcharts
 def changeChartOptions(content):
     content = json.loads(content)
+    # add a range selector
     content['rangeSelector'] = {'buttons':[{'type':'minute','count':24*60,
     'text':'24HR'},{'type':'week','count':1,'text':'1W'},{'type':'month','count':1,
     'text':'1M'},{'type':'month','count':3,'text':'3M'},{'type':'year','count':1,
     'text':'1Y'},{'type':'all','text':'ALL'}]}
+    # disable credits
     content['credits']={'enabled':False}
+    # add y axis title
+    content['yAxis'] = {'title':{'text':'$','rotation':0}}
     content = json.dumps(content)
     return content
+
+# Generates the final link to query yahoo finance on, and the list of additional
+# options specified. See See http://www.jarloo.com/yahoo_finance/ for options
+# Input: 
+# 1. <list> companySym: list of strings of company stock symbols.
+# Output: 
+# 1. <string> finalQuery: string to query yahoo API
+# 2. <list> additionalOptions: The list of additional options to specify to the link
+def getQuery(companySym):
+    """
+    Current options specified are: 
     
-def getQuery(companyNames):
+    symbol / ask / bid / dividend yield / div per share / div pay date / 
+    earnings per share / EPS estimate curr year / EPS est next year / EPS est next quarter
+    EBITDA / p/e ratio / p/b ratio / peg ratio/ short ratio
+    """
+    additionalOptions = ['s','a','b','y','d','r1','e','e7','e8','e9','j4','r','p6','r5','s7']
     base = 'http://finance.yahoo.com/d/quotes.csv?s='
-    companyNamesJoined = '+'.join(companyNames)
-    base = base+companyNamesJoined+'&f='
-    
-    additionalOptions = ['s','a','b','y','d','r1','e','e7','e8','e9','j4','r','s7']
-    #symbol / ask / bid / dividend yield / div per share / div pay date / 
-     #earnings per share / EPS estimate curr year / EPS est next year / EPS est next quarter
-    #EBITDA / p/e ratio / short ratio
-    
+    companySymJoined = '+'.join(companySym)
+    base = base+companySymJoined+'&f='
     finalQuery = base +  ''.join(additionalOptions)
     return finalQuery,additionalOptions
 
+# Generates column names for the pandas column, taken from nameMatch.pkl
+# Input: <List> additionalOptions: the list of additional options
+# Output: <List> columnNames: a list of the full names of the options
 def getColumnNames(additionalOptions):
     with open('nameMatch.pkl', 'rb') as handle:
         referenceDic = pickle.load(handle)
